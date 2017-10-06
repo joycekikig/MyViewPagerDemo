746698d6b9955a116f8214692a42836aae9fb9cb
XiangYunLin <XiangYunLin@zeuscomm.com>
2017-10-06 18:02:44 +0800

    Initial commit

---
+++ app/src/main/java/com/example/xiangyunlin/myviewpagerdemo/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.xiangyunlin.myviewpagerdemo;
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private TabLayout mTablayout;
+    private ViewPager mViewPager;
+    private List<PageView> mPagerViewList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initData();
+        initView();
+    }
+
+    private void initData() {
+        mPagerViewList = new ArrayList<>();
+        mPagerViewList.add(new PageOneView(MainActivity.this));
+        mPagerViewList.add(new PageTwoView(MainActivity.this));
+        mPagerViewList.add(new PageThreeView(MainActivity.this));
+    }
+
+    private void initView() {
+        mTablayout = (TabLayout) findViewById(R.id.tabs);
+        mTablayout.addTab(mTablayout.newTab().setText("Page one"));
+        mTablayout.addTab(mTablayout.newTab().setText("Page two"));
+        mTablayout.addTab(mTablayout.newTab().setText("Page three"));
+
+        mViewPager = (ViewPager) findViewById(R.id.pager);
+        mViewPager.setAdapter(new SamplePagerAdapter());
+        initListener();
+    }
+
+    private void initListener() {
+        mTablayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(mViewPager));
+        mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(mTablayout));
+    }
+
+    private class SamplePagerAdapter extends PagerAdapter {
+
+        @Override
+        public int getCount() {
+            return mPagerViewList.size();
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return view == object;
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            container.addView(mPagerViewList.get(position));
+            return mPagerViewList.get(position);
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            container.removeView((View) object);
+        }
+    }
+
+}
